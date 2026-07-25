5b9d15338997ebc437ffdf37261c36cbf2c59dc6
Mohit Jatav <mohitjatav326@gmail.com>
2026-07-26 01:26:00 +0530

    feat(routes): add forgot-password and reset-password auth routes

--- Interview-Prep/InterviewPrep-Backend/src/routes/authRoutes.js
+++ Interview-Prep/InterviewPrep-Backend/src/routes/authRoutes.js
@@ -1,10 +1,12 @@
 import express from "express";
-import { login, signup, googleLogin } from "../controllers/authController.js";
+import { login, signup, googleLogin, forgotPassword, resetPassword } from "../controllers/authController.js";
 
 const router = express.Router();
 
 router.post("/login", login);
 router.post("/signup", signup);
 router.post("/google-login", googleLogin);
+router.post("/forgot-password", forgotPassword);
+router.post("/reset-password", resetPassword);
 
 export default router;
